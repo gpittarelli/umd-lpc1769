ae5457f641a3bb9a6a9a37b607ab310888f7c7c7
George Pittarelli <g.pittarelli@gmail.com>
2013-03-28 23:31:12 -0400

    Add CMake toolchains files

---
+++ LPCXpressoDir.cmake
@@ -0,0 +1,2 @@
+# Define LPCXPRESSO_DIR here:
+set(LPCXPRESSO_DIR "E:/nxp/LPCXpresso_5.0.14_1109/lpcxpresso")
---
+++ Platform/LPC1769.cmake
@@ -0,0 +1,34 @@
+include(../LPCXpressoDir.cmake)
+
+set(CMAKE_SYSTEM_NAME      Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+if(NOT DEFINED LPCXPRESSO_DIR)
+  message( FATAL_ERROR "******************************
+Must define LPCXPRESSO_DIR pointing to LPCXpresso install directory. Look in LPCXpressoDir.cmake or run cmake with -DLPCXPRESSO_DIR=<lpcxpresso_dir>.
+******************************
+")
+endif()
+
+message(STATUS "Using LPCXpresso Directory: ${LPCXPRESSO_DIR}")
+file(TO_CMAKE_PATH ${LPCXPRESSO_DIR} LPCXPRESSO_DIR)
+set(LPCXPRESSO_GNU_DIR ${LPCXPRESSO_DIR}/tools/bin)
+set(LPCXPRESSO_BIN_DIR ${LPCXPRESSO_DIR}/bin)
+message(STATUS "GNU Toolchain at: ${LPCXPRESSO_GNU_DIR}")
+message(STATUS "LPCXpresso utilities at: ${LPCXPRESSO_BIN_DIR}")
+
+# which compilers to use for C and C++
+find_program(CMAKE_C_COMPILER arm-none-eabi-gcc ${LPCXPRESSO_GNU_DIR})
+#find_program(CMAKE_C_LINK_EXECUTABLE arm-none-eabi-ld ${LPCXPRESSO_GNU_DIR})
+message(STATUS "ARM C compiler: ${CMAKE_C_COMPILER}")
+
+# Compilers like arm-none-eabi-gcc that target bare metal systems don't pass
+# CMake's compiler check, so fill in the results manually and mark the test
+# as passed:
+set(CMAKE_C_COMPILER_ID GNU)
+set(CMAKE_COMPILER_IS_GNUCC 1)
+set(CMAKE_C_COMPILER_ID_RUN TRUE)
+set(CMAKE_C_COMPILER_FORCED TRUE)
+
+include_directories(../CMSISv2p00_LPC17xx/inc)
+link_directories(../CMSISv2p00_LPC17xx/lib)
