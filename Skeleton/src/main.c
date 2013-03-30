1b0500e080aacc764345763015a7ace28c79a9f6
George Pittarelli <g.pittarelli@gmail.com>
2013-03-30 15:27:54 -0400

    Add example/skeleton project

---
+++ Skeleton/src/main.c
@@ -0,0 +1,50 @@
+/*
+ ===============================================================================
+ Name        : main.c
+ Author      :
+ Version     :
+ Description : main definition
+ ===============================================================================
+ */
+
+#ifdef __USE_CMSIS
+#include "LPC17xx.h"
+#endif
+
+#include <cr_section_macros.h>
+#include <NXP/crp.h>
+
+#include "main.h"
+#include "PLL.h"
+
+// Variable to store CRP value in. Will be placed automatically
+// by the linker when "Enable Code Read Protect" selected.
+// See crp.h header for more information
+__CRP const unsigned int CRP_WORD = CRP_NO_CRP;
+
+int main(void) {
+  // Select 12MHz crystal oscillator
+  LPC_SC ->CLKSRCSEL = 1;
+
+  // Bypass PLL 0
+  PLL_bypass();
+
+  // Peripheral power
+  // LPC_SC->PCONP = 0;
+
+  // Peripheral clocks
+  // LPC_SC->PCLKSEL0 = 0;
+  // LPC_SC->PCLKSEL1 = 0;
+
+  // Setup GPIO pins
+  // LPC_GPIO0->FIODIR = 0;
+  // LPC_GPIO0->FIOSET = 0;
+
+  // Select pin modes
+  // LPC_PINCON->PINSEL1 = 0;
+
+  while (1) {
+
+  }
+  return 0;
+}
---
+++ Skeleton/src/main.h
@@ -0,0 +1,9 @@
+#ifndef __MAIN_h_
+#define __MAIN_h_
+
+/* Provides uintN_t, uint_fastN_t, etc. (for N {8,16,32}) */
+#include <stdint.h>
+
+#define _BV(n) (1 << (n))
+
+#endif
