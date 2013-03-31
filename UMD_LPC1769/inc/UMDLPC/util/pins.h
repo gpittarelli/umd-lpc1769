3865ba1484bf563fa9dfd8e81304692832b38a8c
George Pittarelli <g.pittarelli@gmail.com>
2013-03-31 15:44:33 -0400

    Add pin macros header

---
+++ UMD_LPC1769/inc/UMDLPC/util/pins.h
@@ -0,0 +1,50 @@
+/* pins.h
+ *
+ * Define a useful macro for giving GPIO pins nicer, semantic names.
+ *
+ * The only macro exported is DEFINE_PIN. Use as:
+ *  DEFINE_PIN(PIN_NAME, port number, pin number)
+ * this will define the following:
+ *  PIN_NAME_DEASSERT(), _OFF(), _LOW() to bring the pin low
+ *  PIN_NAME_ASSERT(), _ON(), _HIGH() to bring the pin high
+ *  PIN_NAME_INPUT() to make the pin an input
+ *  PIN_NAME_OUTPUT() to make the pin an output
+ *  PIN_NAME_READ() to read the current state of the pin
+ *                  (returns a 1 or 0, typed as a uint_fast8_t)
+ */
+
+#ifndef __UMDLPC_util_pins_h_
+#define __UMDLPC_util_pins_h_
+
+#include <stdint.h>
+
+#define DEFINE_PIN(name, port, pin) \
+inline void name##_DEASSERT() { \
+  LPC_GPIO##port ->FIOCLR |= (1 << pin); \
+} \
+inline void name##_OFF() { \
+  LPC_GPIO##port ->FIOCLR |= (1 << pin); \
+} \
+inline void name##_LOW() { \
+  LPC_GPIO##port ->FIOCLR |= (1 << pin); \
+} \
+inline void name##_ASSERT() { \
+  LPC_GPIO##port ->FIOSET |= (1 << pin); \
+} \
+inline void name##_ON() { \
+  LPC_GPIO##port ->FIOSET |= (1 << pin); \
+} \
+inline void name##_HIGH() { \
+  LPC_GPIO##port ->FIOSET |= (1 << pin); \
+} \
+inline void name##_INPUT() { \
+  LPC_GPIO##port ->FIODIR &= ~(1 << pin); \
+} \
+inline void name##_OUTPUT() { \
+  LPC_GPIO##port ->FIODIR |= (1 << pin); \
+} \
+inline uint_fast8_t name##_OUTPUT() { \
+  return ((( LPC_GPIO##port ->FIOPIN) & (1 << pin)) >> pin);  \
+}
+
+#endif
