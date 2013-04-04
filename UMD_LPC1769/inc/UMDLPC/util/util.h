17ea8dd9a6035c178f25c195bb689018e88dee54
George Pittarelli <george@gpittarelli.com>
2013-04-04 12:20:29 +0000

    add general utilities header

---
+++ UMD_LPC1769/inc/UMDLPC/util/util.h
@@ -0,0 +1,17 @@
+#ifndef __UMDLPC_util_util_h_
+#define __UMDLPC_util_util_h_
+
+#define ARRAY_LEN(a) (sizeof(a)/sizeof((a)[0]))
+
+
+#define MAX(a,b) \
+    ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+       _a > _b ? _a : _b; })
+
+#define MIN(a,b) \
+    ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+       _a < _b ? _a : _b; })
+
+#endif
