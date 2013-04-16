e0fcb616063fdd076faed27f8efeed6467368bc4
George Pittarelli <george@gpittarelli.com>
2013-04-16 21:33:45 +0000

    Add SSD1289 video example, with touch screen

---
+++ SSD1289_Example/src/ssd1289example.h
@@ -0,0 +1,19 @@
+#ifndef __MAIN_h_
+#define __MAIN_h_
+
+/* Provides uintN_t, uint_fastN_t, etc. (for N {8,16,32}) */
+#include <stdint.h>
+
+#ifdef __USE_CMSIS
+#include "LPC17xx.h"
+#endif
+
+#include <cr_section_macros.h>
+#include <NXP/crp.h>
+
+#include "UMDLPC/system/clocking.h"
+#include "pins.h"
+
+#define _BV(n) (1 << (n))
+
+#endif
